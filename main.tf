509e018bf0963d730efb602c6862a252bdb18433
Jeff Spradlin <jeffmspradlin@gmail.com>
2023-06-26 13:59:48 -0500

    adding tfvars file

--- main.tf
+++ main.tf
@@ -7,11 +7,11 @@ resource "vault_policy" "read_policy" {
 data "vault_policy_document" "this" {
   for_each = var.policy
   dynamic "rule" {
-    for_each = each.value.policy[each.key].policy
+    for_each = each.value.policy[*].policy
     content {
-      path         = each.value.path
-      capabilities = each.value.capabilities
-      description  = each.value.description
+      path         = rule.value.path
+      capabilities = rule.value.capabilities
+      description  = rule.value.description
     }
   }
 }
