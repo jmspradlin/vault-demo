e674092ca9455f1fadb65a6e8f0c1ff9136dbdc1
Jeff Spradlin <jeffmspradlin@gmail.com>
2023-06-28 12:04:07 -0500

    updating naming

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "vault_policy" "read_policy" {
   for_each = var.policy
-  name     = each.value.name
+  name     = "tf-${each.value.name}"
   policy   = data.vault_policy_document.this[each.key].hcl
 }
 
