a572459642efb704aefcf12e13d5af0d63a9078d
Jeff Spradlin <jeffmspradlin@gmail.com>
2023-07-13 16:56:29 -0500

    adding entity, user, alias, group and membership

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
+data "vault_auth_backend" "userpass" {
+  path = "userpass"
+}
+
 resource "random_password" "password" {
   length           = 16
   special          = true
@@ -5,8 +9,8 @@ resource "random_password" "password" {
 }
 
 resource "vault_policy" "boundary" {
-  name     = "boundary-${var.boundary_policy.name}"
-  policy   = data.vault_policy_document.boundary.hcl
+  name   = "boundary-${var.boundary_policy.name}"
+  policy = data.vault_policy_document.boundary.hcl
 }
 
 data "vault_policy_document" "boundary" {
@@ -21,11 +25,30 @@ data "vault_policy_document" "boundary" {
 }
 
 resource "vault_generic_endpoint" "end_user" {
-  path = "auth/userpass/users/end-user"
+  path                 = "auth/userpass/users/end-user"
   ignore_absent_fields = true
   data_json = jsonencode({
-    "policies"="boundary-${var.boundary_policy.name}",
-    "password"="${random_password.password.result}",
-    "token_ttl"="1h"
-    })
+    "policies"  = "boundary-${var.boundary_policy.name}",
+    "password"  = "${random_password.password.result}",
+    "token_ttl" = "1h"
+  })
+}
+
+resource "vault_identity_entity" "end_user" {
+  name = "end-user"
+}
+
+resource "vault_identity_group" "engineering" {
+  name = "engineering"
+}
+
+resource "vault_identity_group_member_entity_ids" "members" {
+  member_entity_ids = [vault_identity_entity.end_user.id]
+  group_id          = vault_identity_group.engineering.id
+}
+
+resource "vault_identity_entity_alias" "end_user" {
+  name           = "end-user"
+  mount_accessor = data.vault_auth_backend.userpass.accessor
+  canonical_id   = vault_identity_entity.end_user.id
 }
