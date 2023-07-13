161495c6bf3c5e35355b8e3e6c3f30cfbd9d9c89
Jeff Spradlin <jeffmspradlin@gmail.com>
2023-07-13 12:28:07 -0500

    adding vault userpass auth again

--- provider.tf
+++ provider.tf
@@ -1,13 +1,13 @@
 provider "vault" {
-  #skip_child_token = true
-  # auth_login {
-  #   path      = "auth/approle/login"
-  #   namespace = "admin"
+  skip_child_token = true
+  auth_login {
+    path      = "auth/approle/login"
+    namespace = "admin"
 
 
-  #   parameters = {
-  #     role_id   = var.login_approle_role_id
-  #     secret_id = var.login_approle_secret_id
-  #   }
-  # }
+    parameters = {
+      role_id   = var.login_approle_role_id
+      secret_id = var.login_approle_secret_id
+    }
+  }
 }
