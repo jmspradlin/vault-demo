4f4d52d1e6bdf14f37dc83b82722316925b659ba
Jeff Spradlin <jeffmspradlin@gmail.com>
2023-07-13 17:08:27 -0500

    adding OIDC setup for Boundary

--- main.tf
+++ main.tf
@@ -52,3 +52,37 @@ resource "vault_identity_entity_alias" "end_user" {
   mount_accessor = data.vault_auth_backend.userpass.accessor
   canonical_id   = vault_identity_entity.end_user.id
 }
+
+# Vault OIDC client
+resource "vault_identity_oidc_assignment" "engineering" {
+  name = "engineering-assignment"
+  entity_ids = [
+    vault_identity_entity.end_user.id,
+  ]
+  group_ids = [
+    vault_identity_group.engineering.id,
+  ]
+}
+
+resource "vault_identity_oidc_key" "engineering" {
+  name               = "engineering-key"
+  algorithm          = "RS256"
+  verification_ttl   = "7200"
+  rotation_period    = "3600"
+  allowed_client_ids = ["*"]
+}
+
+resource "vault_identity_oidc_client" "boundary" {
+  name = "boundary"
+  redirect_uris = [
+    "https://01c6c932-2742-4960-86b9-e5070a67d6cc.boundary.hashicorp.cloud:9200/v1/auth-methods/oidc:authenticate:callback",
+    "https://01c6c932-2742-4960-86b9-e5070a67d6cc.boundary.hashicorp.cloud:8251/callback",
+    "https://01c6c932-2742-4960-86b9-e5070a67d6cc.boundary.hashicorp.cloud:8080/callback"
+  ]
+  assignments = [
+    vault_identity_oidc_assignment.engineering.name
+  ]
+  id_token_ttl     = 1800
+  access_token_ttl = 3600
+  key              = vault_identity_oidc_key.engineering.id
+}
