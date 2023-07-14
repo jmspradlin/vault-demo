dd65f2c229e05928cc51f1e0309841322b957c7d
Jeff Spradlin <jeffmspradlin@gmail.com>
2023-07-14 09:50:06 -0500

    insanity check on copied code

--- main.tf
+++ main.tf
@@ -87,22 +87,26 @@ resource "vault_identity_oidc_client" "boundary" {
   key              = vault_identity_oidc_key.engineering.id
 }
 
-resource "vault_identity_oidc_scope" "users" {
-  name = "user"
-  template = jsonencode(
-    {
-      "username" = "{{identity.entity.name}}",
-    }
-  )
-  description = "The user scope provides claims using Vault identity entity metadata"
-}
+# resource "vault_identity_oidc_scope" "users" {
+#   name = "user"
+#   template = jsonencode(
+#     {
+#       username = "{{identity.entity.name}}",
+#     }
+#   )
+#   description = "The user scope provides claims using Vault identity entity metadata"
+# }
 
 resource "vault_identity_oidc_scope" "groups" {
   name = "groups"
   template = jsonencode(
     {
-      groups = "{{identity.entity.groups.names}}",
+      username = "{{identity.entity.name}}",
     }
   )
   description = "The groups scope provides the groups claim using Vault group membership."
 }
+# contact = [{
+#         email        = "{{identity.entity.metadata.email}}",
+#         phone_number = "{{identity.entity.metadata.phone_number}}",
+#       }]
