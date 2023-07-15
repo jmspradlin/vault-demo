f6de4870955208e315602905a3cd87102fd38074
Jeff Spradlin <jeffmspradlin@gmail.com>
2023-07-15 16:03:38 -0500

    removing auth method

--- boundary.tf
+++ boundary.tf
@@ -1,12 +1,12 @@
-resource "boundary_auth_method_oidc" "this" {
-  scope_id           = var.boundary_scope_id
-  issuer             = vault_identity_oidc_provider.this.issuer
-  client_id          = data.vault_identity_oidc_client_creds.boundary.client_id
-  client_secret      = data.vault_identity_oidc_client_creds.boundary.client_secret
-  signing_algorithms = ["RS256"]
-  api_url_prefix     = "${var.boundary_addr}:9200"
-  claims_scopes      = ["groups", "user"]
-  max_age            = 20
-  name               = "Vault OIDC Provider"
-  state              = "active-public"
-}
+# resource "boundary_auth_method_oidc" "this" {
+#   scope_id           = var.boundary_scope_id
+#   issuer             = vault_identity_oidc_provider.this.issuer
+#   client_id          = data.vault_identity_oidc_client_creds.boundary.client_id
+#   client_secret      = data.vault_identity_oidc_client_creds.boundary.client_secret
+#   signing_algorithms = ["RS256"]
+#   api_url_prefix     = "${var.boundary_addr}:9200"
+#   claims_scopes      = ["groups", "user"]
+#   max_age            = 20
+#   name               = "Vault OIDC Provider"
+#   state              = "active-public"
+# }
