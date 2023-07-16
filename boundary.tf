7cb7c3049d44058e5040a4a3a8c95ab774e4d28d
Jeff Spradlin <jeffmspradlin@gmail.com>
2023-07-16 18:31:27 -0500

    setting as primary for scope

--- boundary.tf
+++ boundary.tf
@@ -1,13 +1,13 @@
 resource "boundary_auth_method_oidc" "this" {
-  scope_id             = var.boundary_scope_id
-  issuer               = vault_identity_oidc_provider.this.issuer
-  client_id            = data.vault_identity_oidc_client_creds.boundary.client_id
-  client_secret        = data.vault_identity_oidc_client_creds.boundary.client_secret
-  signing_algorithms   = ["RS256"]
-  api_url_prefix       = var.boundary_addr
-  claims_scopes        = ["groups", "user"]
-  max_age              = 20
-  name                 = "Vault OIDC Provider"
-  state                = "active-public"
+  scope_id           = "global" #var.boundary_scope_id
+  issuer             = vault_identity_oidc_provider.this.issuer
+  client_id          = data.vault_identity_oidc_client_creds.boundary.client_id
+  client_secret      = data.vault_identity_oidc_client_creds.boundary.client_secret
+  signing_algorithms = ["RS256"]
+  api_url_prefix     = var.boundary_addr
+  claims_scopes      = ["groups", "user"]
+  max_age            = 20
+  name               = "Vault OIDC Provider"
+  state              = "active-public"
   is_primary_for_scope = true
 }
